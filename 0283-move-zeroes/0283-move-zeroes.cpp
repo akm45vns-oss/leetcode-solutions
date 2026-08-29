18e43776243c03814cc4a220dfd36b50c30b35d7
Ayush Kumar Maurya <akm45.vns@gmail.com>
2026-08-29 12:19:19 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.92%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int s=0;
+        for(int i=0;i<nums.size();i++){
+           if(nums[i]!=0){
+            int temp=nums[i];
+            nums[i]=nums[s];
+            nums[s]=temp;
+            s++;
+        }
+        }
+
+    }
+};
