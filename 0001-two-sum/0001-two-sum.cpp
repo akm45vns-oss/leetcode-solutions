e3e583244c5dcd113051b1d738f80f527e77f81e
Ayush Kumar Maurya <akm45.vns@gmail.com>
2026-09-04 15:58:03 +0530

    Time: 45 ms (14.81%), Space: 14.1 MB (88.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>ans(2);
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target && i!=j){
+                if(nums[i]+nums[j]==target){
                     ans[0]=i;
                     ans[1]=j;
                     break;
