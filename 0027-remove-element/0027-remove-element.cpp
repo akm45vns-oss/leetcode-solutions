f5117913be2e78aff9c566def8ae435424ab983a
Ayush Kumar Maurya <akm45.vns@gmail.com>
2026-09-11 15:45:49 +0530

    Time: 0 ms (100%), Space: 11.7 MB (84.25%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,11 +4,33 @@ public:
         int c=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
-                nums[c]=nums[i];
+            nums[c]=nums[i];
             c++;
             }
         }
-   
     return c;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
